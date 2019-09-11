8655d5f78d7f18881a3514a4349be8f83c6fefd4
Natalia <nathy.andreola@gmail.com>
2019-09-11 17:34:52 -0400

    added libraries

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCwM6JIo0ZayO4MFqgMJeadg7PqWRe1QEs",
+    authDomain: "bingefest-1b070.firebaseapp.com",
+    databaseURL: "https://bingefest-1b070.firebaseio.com",
+    projectId: "bingefest-1b070",
+    storageBucket: "",
+    messagingSenderId: "49592450024",
+    appId: "1:49592450024:web:96a2c6e27974bf3c7b825d"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
