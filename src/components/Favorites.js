8283e7e2588fcd47fd10b0bd4c8ae3296fd8d1e2
Nicholas Chug <nicholas.chug@gmail.com>
2019-09-13 12:56:37 -0400

    added updated favorites file wooooooo

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,22 +1,30 @@
 import React, { Component } from 'react';
 import ShowCard from './ShowCard';
 import RestaurantCard from './RestaurantCard';
-import firebase from './firebase';
+import firebase from '../firebase';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Mix from "./Mix";
 
 class Favourites extends Component {
+
+    getFavouriteShows = () => {
+        
+    }
+
+    getFavouriteRestaurants = () => {
+
+    }
 
     render() {
         return (
             <Router>
                 <div>
                     <h1>Get your Favorite TV Shows!</h1>
-                    <button>Show Favorite Shows</button>
+                    <button onClick={this.getFavouriteShows}>Show Favorite Shows</button>
                 </div>
                 <div>
                     <h1>Get your Favorite Restaurant!</h1>
-                    <button>Show Favorite Restaurant</button>
+                    <button onClick={this.getFavouriteRestaurants}>Show Favorite Restaurant</button>
                 </div>
                 <div>
                     <h1>Get your Favorite Combos!</h1>
@@ -24,9 +32,9 @@ class Favourites extends Component {
                 </div>
                 <div>
                     <h2>Click below to get your generated combo</h2>
-                    <Link to="/mix">Mix</Link>
-                    <Route path="/mix" component={Mix}/>
-                </div>           
+                    <button><Link to="/mix">Get Your Combo!</Link></button>
+                    <Route exact path="/mix" component={Mix}/>
+                </div>  
             </Router>
         );
     };
