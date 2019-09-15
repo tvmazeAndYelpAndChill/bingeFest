2e0b48891b846f2b935ef50c76ef7baab15dd92d
Danii Shen <s2d.shen@gmail.com>
2019-09-15 15:58:12 -0400

    style updates to header

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -6,31 +6,28 @@ class MainHeader extends Component {
             <div>
                 <header className="mainHeader">
                     <div className="wrapper">
-                        <div className="tvForm">
-                            <input onChange={this.props.handleChange} onKeyUp={this.props.getTvShows} placeholder="tvshows" type="text" onKeyPress={this.props.handleKeyEnterTV} />
-                        </div>
-                        <div className="restoForm">
-                            <input onChange={this.props.handleChange} onKeyUp={this.props.getRestaurants} placeholder="restos" type="text" onKeyPress={this.props.handleKeyEnterResto} />
-                        </div>
-
-
-                        <ul>
-                            {
-                                this.props.searchedShows.map(((match, index) => {
-                                    return (
-                                        <li key={index} value={index} onClick={this.props.handlePressTv}>{match.name}</li>
-                                    )
-                                }))}
-                        </ul>
-
-                        <ul>
-                            {
-                                this.props.searchedRestaurants.map(((match, index) => {
-                                    return (
-                                        <li key={index} value={index} onClick={this.props.handlePressResto} >{match.name}</li>
-                                    )
-                                }))}
-                        </ul>
+                        <form className="tvForm">
+                            <input className="tvInput" onChange={this.props.handleChange} onKeyUp={this.props.getTvShows} placeholder="tvshows" type="text" onKeyPress={this.props.handleKeyEnterTV} />
+                            <ul>
+                                {
+                                    this.props.searchedShows.map(((match, index) => {
+                                        return (
+                                            <li key={index} value={index} onClick={this.props.handlePressTv}>{match.name}</li>
+                                        )
+                                    }))}
+                            </ul>
+                        </form>
+                        <form className="restoForm">
+                            <input className="restoInput" onChange={this.props.handleChange} onKeyUp={this.props.getRestaurants} placeholder="restos" type="text" onKeyPress={this.props.handleKeyEnterResto} />
+                            <ul>
+                                {
+                                    this.props.searchedRestaurants.map(((match, index) => {
+                                        return (
+                                            <li key={index} value={index} onClick={this.props.handlePressResto} >{match.name}</li>
+                                        )
+                                    }))}
+                            </ul>
+                        </form>
                     </div>
                 </header>
             </div>
