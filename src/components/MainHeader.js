2c80d59f8297ba72d510fbe64c93530dac535a31
Danii Shen <s2d.shen@gmail.com>
2019-09-16 10:32:17 -0400

    header complete :pizza:

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -5,29 +5,42 @@ class MainHeader extends Component {
         return (
             <div>
                 <header className="mainHeader">
-                    <div className="wrapper">
-                        <form className="tvForm">
-                            <input className="tvInput" onChange={this.props.handleChange} onKeyUp={this.props.getTvShows} placeholder="tvshows" type="text" onKeyPress={this.props.handleKeyEnterTV} />
-                            <ul>
-                                {
-                                    this.props.searchedShows.map(((match, index) => {
-                                        return (
-                                            <li key={index} value={index} onClick={this.props.handlePressTv}>{match.name}</li>
-                                        )
-                                    }))}
-                            </ul>
-                        </form>
-                        <form className="restoForm">
-                            <input className="restoInput" onChange={this.props.handleChange} onKeyUp={this.props.getRestaurants} placeholder="restos" type="text" onKeyPress={this.props.handleKeyEnterResto} />
-                            <ul>
-                                {
-                                    this.props.searchedRestaurants.map(((match, index) => {
-                                        return (
-                                            <li key={index} value={index} onClick={this.props.handlePressResto} >{match.name}</li>
-                                        )
-                                    }))}
-                            </ul>
-                        </form>
+                    <div className="wrapperHeader">
+                        <div className="tvForm">
+                            <form>
+                                <div className="inputBar">
+                                    <input className="tvInput" onChange={this.props.handleChange} onKeyUp={this.props.getTvShows} placeholder="Search for a TV Show" type="text" onKeyPress={this.props.handleKeyEnterTV} />
+                                    <button className="searchButton">🡢</button>
+
+                                </div>
+                                <ul>
+                                    {
+                                        this.props.searchedShows.map(((match, index) => {
+                                            return (
+                                                <li key={index} value={index} onClick={this.props.handlePressTv}>{match.name}</li>
+                                            )
+                                        }))}
+                                </ul>
+                            </form>
+                        </div>
+                        <div className="restoForm">
+
+                            <form>
+                                <div className="inputBar">
+                                    <input className="restoInput" onChange={this.props.handleChange} onKeyUp={this.props.getRestaurants} placeholder="Search for a restaurant" type="text" onKeyPress={this.props.handleKeyEnterResto} />
+                                    <button className="searchButton">🡢</button>
+                                </div>
+                                
+                                <ul>
+                                    {
+                                        this.props.searchedRestaurants.map(((match, index) => {
+                                            return (
+                                                <li key={index} value={index} onClick={this.props.handlePressResto} >{match.name}</li>
+                                            )
+                                        }))}
+                                </ul>
+                            </form>
+                        </div>
                     </div>
                 </header>
             </div>
